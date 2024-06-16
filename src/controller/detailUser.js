63891fc3303fd2603bcf3072bba4e0562c973e1d
YusufNugroho207 <yusufnugroho207@gmail.com>
2024-06-16 21:13:11 +0700

    Update Photo Profile Upload

--- src/controller/detailUser.js
+++ src/controller/detailUser.js
@@ -1,4 +1,5 @@
 const prisma = require("../prisma");
+const multer = require('multer');
 const ClientError = require("../exceptions/ClientError");
 const InputError = require("../exceptions/InputError");
 
@@ -29,29 +30,33 @@ const getUserInfo = async (req, res) => {
 
 const updateProfilePhoto = async (req, res) => {
   try {
-    const { profile_Photo } = req.body;
-
-    if (!profile_Photo) {
-      throw new InputError("Profile photo is required");
+    if (!req.file) {
+      throw new InputError('Profile photo is required');
     }
+
+    const profilePhoto = req.file.encoding;
 
     const updatedUser = await prisma.user.update({
       where: { id: req.user.userId },
-      data: { profile_Photo },
+      data: { profile_Photo: profilePhoto },
     });
 
     res.status(200).json({
-      message: "Profile photo updated successfully",
+      message: 'Profile photo updated successfully',
       updatedUser,
     });
   } catch (error) {
-    if (error instanceof ClientError) {
-      res.status(error.statusCode).json({ error: error.message });
-    } else if (error.code === "P2025") {
-      res.status(404).json({ error: "User not found" });
+    if (error instanceof InputError || error.code === 'P2025') {
+      res.status(400).json({ error: error.message });
+    } else if (error instanceof multer.MulterError) {
+      if (error.code === 'LIMIT_FILE_SIZE') {
+        res.status(400).json({ error: 'File size exceeds limit of 5MB' });
+      } else {
+        res.status(500).json({ error: 'Multer error: ' + error.message });
+      }
     } else {
-      console.error("Error updating profile photo:", error);
-      res.status(500).json({ error: "Internal server error" });
+      console.error('Error updating profile photo:', error);
+      res.status(500).json({ error: 'Internal server error' });
     }
   }
 };
--- src/routes/detailUser.js
+++ src/routes/detailUser.js
@@ -1,11 +1,12 @@
 const express = require('express');
 const authenticateToken = require('../middleware/authToken');
 const detailController = require('../controller/detailUser');
+const upload  = require('../middleware/uploadPhoto');
 
 const router = express.Router();
 
 router.get('/user-info', authenticateToken, detailController.getUserInfo);
 
-router.put('/update-photo', authenticateToken, detailController.updateProfilePhoto);
+router.put('/update-photo', authenticateToken, upload.single('profile_Photo'), detailController.updateProfilePhoto);
 
 module.exports = router;
